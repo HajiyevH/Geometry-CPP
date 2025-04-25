f13a6f133d30d68fd2fd6a7cd6f2a6e997779b94
Hajiaga Hajiyev <hajiyevhajiaga0@gmail.com>
2025-04-25 19:24:24 +0200

    First files

---
+++ distanceCheck.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+struct sPoint2D
+{
+sPoint2D(float xValue, float yValue)
+    {
+    x = xValue;
+    y = yValue;
+    }
+float x;
+float y;
+};
+int main() {
+
+}
